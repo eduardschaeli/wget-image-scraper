c7f83c372236edacf46dab1ab3bad150fe4efb8f
Eduard Schaeli <eduard.schaeli@benthoo.ch>
2012-11-02 11:17:19 +0100

    Add Readme and subdirectory for the downloads

---
+++ README.md
@@ -0,0 +1,15 @@
+# Image scraper
+
+This shellscript scrapes all images, recursively, from the urls listed
+in the sites.txt file.
+
+It will be used in an art project by Stefan Baltensperger.
+
+Usage:
+  - Add urls to the sites.txt file like this
+      http://www.tagi.ch
+    The HTTP:// part ist important!
+  - Make the scraper.sh shell script executable:
+      chmod +x scraper.sh
+  - Run it like this:
+      ./scraper.sh
--- scraper.sh
+++ scraper.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
+mkdir downloads
 for line in `cat sites.txt`; do
   # replace http://
   stripped_url=`echo $line| cut -c8-`
-  target_folder=$stripped_url
+  target_folder="downloads/$stripped_url"
 
   echo $stripped_url
-  mkdir $stripped_url
+  mkdir $target_folder
   echo ""
   echo ""
   echo ""
   echo "Scraping $stripped_url"
   echo "-----------------------------------"
   echo "> creating folder.."
-  mkdir $stripped_url
+  mkdir $target_folder
   echo "> scraping $stripped_url"
   wget -e robots=off --recursive -p \
     -nd -nc -np --accept jpg,jpeg,png,gif -P $target_folder --wait 0.5 $stripped_url
